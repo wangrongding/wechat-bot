c4260b723533f264d6b72879b55117f940eb0cfe
herohr0209 <92671865+herohr0209@users.noreply.github.com>
2022-12-12 13:01:23 +0000

    Pending changes exported from your codespace

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // 定义机器人的名称，这里是为了防止群聊消息太多，所以只有艾特机器人才会回复，
-export const botName = '荣顶'
+export const botName = '@小R'
 
 // 群聊白名单，白名单内的群聊才会自动回复
-export const roomWhiteList = ['群名称']
+export const roomWhiteList = ['三零三']
 
 // 联系人白名单，白名单内的联系人才会自动回复
-export const aliasWhiteList = ['备注名或微信名', '张三', '李四']
+export const aliasWhiteList = ['0x1D']
