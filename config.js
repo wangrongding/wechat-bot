e96566cf93bca0c8de3fbe9837f855bc6d4ef264
荣顶 <wangrongding@qq.com>
2024-03-27 22:11:48 +0800

    fix: 完善配置文件

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 // 定义机器人的名称，这里是为了防止群聊消息太多，所以只有艾特机器人才会回复，
-export const botName = '荣顶'
+// 这里不要把@去掉，在@后面加上你启动机器人账号的微信名称
+export const botName = '@荣顶'
 
 // 群聊白名单，白名单内的群聊才会自动回复
-export const roomWhiteList = ['群名称']
+export const roomWhiteList = ['群名称', '前端超人⑤【WeChat-Bot】']
 
 // 联系人白名单，白名单内的联系人才会自动回复
 export const aliasWhiteList = ['备注名或微信名', '张三', '李四']
