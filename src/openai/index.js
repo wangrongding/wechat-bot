8a315a7e9c29f29d743dfdf497f5ec376e4306d9
荣顶 <wangrongding@qq.com>
2024-07-24 16:53:45 +0800

    fix: 完善启动服务时,没有 env 文件的不友好报错

--- src/openai/index.js
+++ src/openai/index.js
@@ -3,6 +3,16 @@ import stripMarkdown from 'strip-markdown'
 import OpenAIApi from 'openai'
 import dotenv from 'dotenv'
 const env = dotenv.config().parsed // 环境参数
+import fs from 'fs'
+import path from 'path'
+
+const __dirname = path.resolve()
+// 判断是否有 .env 文件, 没有则报错
+const envPath = path.join(__dirname, '../.env')
+if (!fs.existsSync(envPath)) {
+  console.log('❌ 请先根据文档，创建并配置.env文件！')
+  process.exit(1)
+}
 
 let config = {
   apiKey: env.OPENAI_API_KEY,
